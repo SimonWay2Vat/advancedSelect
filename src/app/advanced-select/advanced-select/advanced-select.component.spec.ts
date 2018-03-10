5bbdce188459a90dce87f4d729da96a9854e94f3
Ariel <ariel.henryson@gmail.com>
2018-03-10 14:36:03 +0200

    init

--- src/app/advanced-select/advanced-select/advanced-select.component.spec.ts
+++ src/app/advanced-select/advanced-select/advanced-select.component.spec.ts
@@ -1,23 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { AdvancedSelectComponent } from './advanced-select.component'
+import { MatAutocompleteModule, MatChipsModule, MatIconModule, MatInputModule } from '@angular/material'
+import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
+import { OrderByPipe } from './orderBy.pipe'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 describe('AdvancedSelectComponent', () => {
-  let component: AdvancedSelectComponent;
-  let fixture: ComponentFixture<AdvancedSelectComponent>;
+  let component: AdvancedSelectComponent
+  let fixture: ComponentFixture<AdvancedSelectComponent>
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdvancedSelectComponent ]
+      declarations: [
+        AdvancedSelectComponent,
+        OrderByPipe
+      ],
+      imports: [
+        CommonModule,
+        MatAutocompleteModule,
+        MatChipsModule,
+        MatInputModule,
+        MatIconModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents()
   }))
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AdvancedSelectComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
+
 
   it('should create', () => {
     expect(component).toBeTruthy()
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed, async } from '@angular/core/testing'
 import { AppComponent } from './app.component'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       declarations: [
         AppComponent
       ],
+      imports: []
     }).compileComponents()
   }))
+
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent)
